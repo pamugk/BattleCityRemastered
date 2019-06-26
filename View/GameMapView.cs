f9279d2f7ee71cab8f58e898855817f2973d246e
pamugk <48994871+pamugk@users.noreply.github.com>
2019-06-26 21:24:12 +0500

    Added class stub for game view.

---
+++ View/GameMapView.cs
@@ -0,0 +1,13 @@
+﻿using Model.Map;
+using System.Drawing;
+
+namespace View
+{
+    public static class GameMapView
+    {
+        public static Image DrawMap(Map gameMap)
+        {
+            return null;
+        }
+    }
+}
---
+++ View/ImagingExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using Imaging = System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using static System.Windows.Interop.Imaging;
+
+namespace View
+{
+    static class ImagingExtensions
+    {
+        public static Bitmap ToBitmap(this BitmapSource bitmapsource)
+        {
+            var width = bitmapsource.PixelWidth;
+            var height = bitmapsource.PixelHeight;
+            var stride = width * ((bitmapsource.Format.BitsPerPixel + 7) / 8);
+            var memoryBlockPointer = Marshal.AllocHGlobal(height * stride);
+            bitmapsource.CopyPixels(new Int32Rect(0, 0, width, height), memoryBlockPointer, height * stride, stride);
+            var bitmap = new Bitmap(width, height, stride, Imaging.PixelFormat.Format32bppArgb, memoryBlockPointer);
+            return bitmap;
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool DeleteObject([In] IntPtr hObject);
+
+        public static BitmapSource ToImageSource(this Bitmap bitmap)
+        {
+            var handle = bitmap.GetHbitmap();
+            try
+            {
+                return CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                DeleteObject(handle);
+            }
+        }
+    }
+}
